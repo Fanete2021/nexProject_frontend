052f18d33003ee30de2ea475a5e20953d0767542
n.goncharov <n.goncharov@powwwer.io>
2025-04-19 10:59:11 +0700

    Фикс отображения иконок для сафари #5

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       include: '**/*.svg',
       svgrOptions: {
         exportType: 'default',
-        icon: false
+        icon: true
       }
     }),
     babel()
