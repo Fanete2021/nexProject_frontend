f1ddf5b388f5a10945c86261e5d661a7a5b62634
n.goncharov <n.goncharov@powwwer.io>
2025-04-25 13:43:20 +0700

    Поменял путь api и добавил https в vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 import babel from 'vite-plugin-babel';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [
@@ -29,6 +30,17 @@ export default defineConfig({
   server: {
     host: 'nex.moootvey.ru',
     port: 3000,
+    https: {
+      key: fs.readFileSync('C:\\cert\\privateKey.key'),
+      cert: fs.readFileSync('C:\\cert\\certificate.crt'),
+      ciphers: 'TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256',
+      minVersion: 'TLSv1.2'
+    },
+    hmr: {
+      host: 'nex.moootvey.ru',
+      port: 3000,
+      protocol: 'wss'
+    }
     // allowedHosts: [
     //     '.ngrok-free.app',
     // ],
